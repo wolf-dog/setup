22b850159f43a5f0e3b4da765a9a5fc8312e4003
Takaaki Hirano <hivedark@gmail.com>
2016-05-08 14:36:08 +0900

    On fedora, install `libcurl-devel` for git.

--- distro/fedora/git.sh
+++ distro/fedora/git.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd $(dirname $0)
 
-sudo dnf install -y wget make autoconf gcc zlib-devel perl-ExtUtils-Embed
+sudo dnf install -y wget make autoconf gcc zlib-devel perl-ExtUtils-Embed libcurl-devel
 
 cd ../../app/git
 ./install.sh
