27cbbb173504d5b3bfd41f52c567224a7e848387
Takaaki Hirano <hivedark@gmail.com>
2016-10-05 12:30:53 +0900

    Migrated from neobundle to dein.

--- app/dotfiles/install.sh
+++ app/dotfiles/install.sh
@@ -8,10 +8,8 @@ ln -s dotfiles/.gitconfig
 ln -s dotfiles/.gitignore
 ln -s dotfiles/.vim
 ln -s dotfiles/.vimrc
-cd .vim
-mkdir bundle
-cd bundle
-git clone git@github.com:Shougo/neobundle.vim
-vim +NeoBundleInstall +q
-
+mkdir ~/.cache/dein/repos/github.com/Shougo/dein.vim
+cd ~/.cache/dein/repos/github.com/Shougo/dein.vim
+git clone git@github.com:Shougo/dein.vim
+vim +call dein#install() +q
 
