003cb4a0dd36a37815e3cb3823a01b5f8345d197
Takaaki Hirano <hivedark@gmail.com>
2016-10-26 11:05:32 +0900

    Modified vim installer for ubuntu.

--- distro/ubuntu/vim.sh
+++ distro/ubuntu/vim.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd $(dirname $0)
 
-sudo apt-get install -y ncurses-dev libluajit-5.1
+sudo apt-get install -y libluajit-5.1 luajit python-dev ncurses-dev
 
 cd ../../app/vim
 ./install.sh
