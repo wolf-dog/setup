47d05e58fc53508217e103ea7d28e6d180c7e0d4
Takaaki Hirano <takaaki.hirano.666@gmail.com>
2017-02-07 18:23:59 +0900

    Bade farewell to dein.vim.

--- app/dotfiles/install.sh
+++ app/dotfiles/install.sh
@@ -3,13 +3,14 @@ cd
 git clone git@github.com:wolf-dog/dotfiles
 \rm .bashrc
 ln -s dotfiles/.bashrc
+\rm .ctags
 ln -s dotfiles/.ctags
+\rm .gitconfig
 ln -s dotfiles/.gitconfig
+\rm .gitignore
 ln -s dotfiles/.gitignore
+\rm .vim
 ln -s dotfiles/.vim
+\rm .vimrc
 ln -s dotfiles/.vimrc
-mkdir ~/.cache/dein/repos/github.com/Shougo/dein.vim
-cd ~/.cache/dein/repos/github.com/Shougo/dein.vim
-git clone git@github.com:Shougo/dein.vim
-vim +call dein#install() +q
 
