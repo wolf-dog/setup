961850684449018c58273b2cadd9982e64e7aac6
Takaaki Hirano <hivedark@gmail.com>
2017-03-23 18:35:38 +0900

    Modified dependencies on fedora.

--- distro/fedora/git.sh
+++ distro/fedora/git.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd $(dirname $0)
 
-sudo dnf install -y wget make autoconf gcc zlib-devel perl-ExtUtils-Embed libcurl-devel
+sudo dnf install -y wget make autoconf gcc zlib-devel perl-ExtUtils-Embed libcurl-devel tcl gettext
 
 ../../app/git/install.sh
 
